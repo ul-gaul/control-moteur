ab761ab88d546e1aa5e79ace83123641a76e5304
Maxime Guillemette <maxime_glmt@hotmail.ca>
2020-01-24 21:44:12 -0500

    added src project for motor control repo

---
+++ motor_control.X/main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+int main(void) {
+    
+    
+    for(;;) {
+        
+    }
+    
+    return 0;
+}
---
+++ motor_control.X/main.h
@@ -0,0 +1,15 @@
+/*
+ * Main source file for the motor control system of the rocket
+ * 
+ * Authors:
+ *      Simon Gaudy
+ *      Maxime Guillemette
+ *      Elliot Lafront
+ * 
+ * Brief:
+ *      TODO
+ */
+
+
+/* standard lib includes */
+#include "xc.h"
