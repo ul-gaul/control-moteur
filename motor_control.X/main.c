bbc30debe9419ccc59281a2d9626a907caf1ff4d
Maxime Guillemette <maxime_glmt@hotmail.ca>
2020-03-07 15:30:23 -0500

    UART reception working

--- motor_control.X/main.c
+++ motor_control.X/main.c
@@ -17,26 +17,24 @@ int main(void) {
 	U1MODEbits.ON = 1;
 	U1STAbits.URXEN = 1;
 	U1STAbits.UTXEN = 1;
-	U1BRG = 13; // TODO adjust this later to get the baudrate right
-	
-  
+	U1BRG = 82;
+
 	i = 0;
 	j = 0;
-	
+
 	actuator_init();
-	
 
 	for(;;) {
-		while (U1STAbits.URXDA == 0);
-		buf[i] = U1RXREG;
-		i ++;
-		if (i >= 6) {
-			i = 0;
-			unpack_command_packet(&cmdpkt, buf);
-			/* TODO: flip gpios to light LEDs according to the received command */
+		while (U1STAbits.RIDLE == 1);
+		while (U1STAbits.URXDA == 1) {
+			buf[i++] = U1RXREG;
+			if (i == 32) {
+				i = 0;
+			}
 		}
-		actuator_set(i++, (i % 2 == 0));
-		
+		unpack_command_packet(&cmdpkt, buf);
+		/* TODO: flip gpios to light LEDs according to the received command */
+		actuator_set(i, (i % 2 == 0));
 	}
 
 	return 0;
